f267eb5a9f57bf5b5baba83451ff49a22c25d9a0
mk3058 <kmk3058@icloud.com>
2023-11-13 23:15:01 +0900

    fix(OrderService): MenuCategory에서 throw된 exception을 catch 하도록 수정

--- src/main/java/christmas/user/service/OrderService.java
+++ src/main/java/christmas/user/service/OrderService.java
@@ -2,6 +2,7 @@ package christmas.user.service;
 
 import christmas.menu.domain.MenuCategory;
 import christmas.menu.domain.MenuItem;
+import christmas.message.Error;
 import christmas.user.domain.Order;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +12,11 @@ public class OrderService {
     public Order newOrder(Map<String, Integer> input) {
         Map<MenuItem, Integer> order = new HashMap<>();
 
-        input.forEach((key, value) -> order.put(MenuCategory.findMenuItemByName(key), value));
+        try {
+            input.forEach((key, value) -> order.put(MenuCategory.findMenuItemByName(key), value));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(Error.INVALUD_ORDER.toString());
+        }
         return new Order(order);
     }
 }
