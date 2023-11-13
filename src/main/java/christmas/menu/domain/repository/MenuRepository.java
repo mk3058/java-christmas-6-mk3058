901a804d5f43b48b347b71484b5aa208f1364223
mk3058 <kmk3058@icloud.com>
2023-11-13 15:39:47 +0900

    refactor: Menu 클래스와 MenuRepository 클래스 분리

--- src/main/java/christmas/menu/domain/repository/MenuRepository.java
+++ src/main/java/christmas/menu/domain/repository/MenuRepository.java
@@ -1,25 +1,13 @@
 package christmas.menu.domain.repository;
 
-import christmas.menu.domain.Appetizer;
-import christmas.menu.domain.Beverage;
-import christmas.menu.domain.Desert;
-import christmas.menu.domain.Main;
+import christmas.menu.domain.Menu;
 import christmas.menu.domain.MenuItem;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class MenuRepository {
 
-    private final List<MenuItem> items = new ArrayList<>();
-
-    public MenuRepository() {
-        addItemsByCategory(Appetizer.class);
-        addItemsByCategory(Main.class);
-        addItemsByCategory(Desert.class);
-        addItemsByCategory(Beverage.class);
-    }
+    private final Menu menu = new Menu();
 
     public List<MenuItem> findItemsByCategory(Class<? extends Enum<?>> category) {
         return items.stream()
@@ -29,12 +17,5 @@ public class MenuRepository {
 
     public Optional<MenuItem> findItemByName(String name) {
         return items.stream().filter(item -> item.getName().equals(name)).findFirst();
-    }
-
-    private <E extends Enum<E> & MenuItem> void addItemsByCategory(Class<E> enumClass) {
-        items.addAll(
-                Arrays.stream(enumClass.getEnumConstants())
-                        .toList()
-        );
     }
 }
