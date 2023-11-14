6d96546a44ca1e5eabaeadb69233b89998a368a5
mk3058 <kmk3058@icloud.com>
2023-11-14 15:10:55 +0900

    feat(SpecialDiscount): SpecialDiscount 추가

---
+++ src/main/java/christmas/event/domain/SpecialDiscount.java
@@ -0,0 +1,36 @@
+package christmas.event.domain;
+
+import christmas.user.domain.Order;
+import christmas.user.domain.VisitDate;
+import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class SpecialDiscount extends Event {
+
+    private static final BigDecimal DISCOUNT_AMOUNT = new BigDecimal(1_000);
+    private static final LocalDate CHRISTMAS = LocalDate.of(2023, 12, 25);
+
+    public SpecialDiscount() {
+        this.eventName = "특별 할인";
+        this.startAt = LocalDate.of(2023, 12, 1);
+        this.endAt = LocalDate.of(2024, 1, 1);
+    }
+
+    @Override
+    public boolean isEligible(VisitDate visitDate, Order order) {
+        if (!hasCorrectAmountAndDate(visitDate, order) || !isSundayOfChristmas(visitDate)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public BigDecimal getBenefitAmount(VisitDate visitDate, Order order) {
+        return DISCOUNT_AMOUNT;
+    }
+
+    private boolean isSundayOfChristmas(VisitDate visitDate) {
+        return visitDate.getDayOfWeek() == DayOfWeek.SUNDAY || visitDate.getDate().isEqual(CHRISTMAS);
+    }
+}
