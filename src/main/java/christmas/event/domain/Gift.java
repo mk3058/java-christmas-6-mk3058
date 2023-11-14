28db27b07bb9f3cc8b8ffea33d065bab0e45967a
mk3058 <kmk3058@icloud.com>
2023-11-14 18:55:21 +0900

    fix(Gift): 생성자 추가

--- src/main/java/christmas/event/domain/Gift.java
+++ src/main/java/christmas/event/domain/Gift.java
@@ -3,12 +3,19 @@ package christmas.event.domain;
 import christmas.user.domain.Order;
 import christmas.user.domain.VisitDate;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Gift extends Event {
 
     private static final String GIFT_NAME = "샴페인";
     private static final BigDecimal BENEFIT_AMOUNT = BigDecimal.valueOf(25_000);
     private static final BigDecimal BENEFIT_THRESHOLD = BigDecimal.valueOf(120_000);
+
+    public Gift() {
+        this.eventName = "증정 이벤트";
+        this.startAt = LocalDate.of(2023, 12, 1);
+        this.endAt = LocalDate.of(2024, 1, 1);
+    }
 
     @Override
     public boolean isEligible(VisitDate visitDate, Order order) {
